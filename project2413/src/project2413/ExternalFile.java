bd0191bda05acb3d1c4bf6b2109e1784d250ab5a
Orion Jamal <orionjamal@gmail.com>
2024-10-23 01:40:35 -0700

    Added folder structure for project for easy viewing

---
+++ project2413/src/project2413/ExternalFile.java
@@ -0,0 +1,13 @@
+package project2413;
+
+
+public class ExternalFile{
+	
+	public static void main(String[] args) {
+		
+		TestClass tc = new TestClass("Testing");
+		
+		tc.test();
+		
+	}
+}
---
+++ project2413/src/project2413/TestClass.java
@@ -0,0 +1,19 @@
+package project2413;
+
+public class TestClass {
+	
+	//Will try different packages
+	protected String msg;
+	
+	public TestClass(String msg) {
+		
+		this.msg = msg;
+	}
+	
+	public void test() {
+		
+		System.out.println(this.msg);
+		
+	}
+
+}
