b2146b96420df283e67669848d8894af3634446d
Orion Jamal <orionjamal@gmail.com>
2024-10-30 03:21:27 -0700

    Connected to database and tried a test query

--- project2413/src/project2413/HealthSystem.java
+++ project2413/src/project2413/HealthSystem.java
@@ -1,5 +1,11 @@
 package project2413;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 
 public class HealthSystem{
 	
@@ -7,22 +13,94 @@ public class HealthSystem{
 	
 	private String version;
 	
+	private String url; //for connecting to db using conn and 
+	
+	private String query; //for writing sql queries
+	
+	private Connection conn; //for creating statements
+	
+	private Statement exQ; //for executing queries
+	
+	private ResultSet rs; //for executing SELECT queries
+	
 	private boolean isAuthenticated;
+	
+	
 	
 	
 	//add args below (exam, db, etc.)
 	
-	public boolean db_open(/*db database*/) { 
+	public HealthSystem() {
 		
-		return true;
-	
+		
+		this.url = "jdbc:sqlite:Database.db";
+		
 	}
 	
+
+	
+	
+	
+	public boolean db_open() { 
+	
+		
+		try {
+			
+			this.conn = DriverManager.getConnection(this.url);
+			this.exQ = this.conn.createStatement();
+		
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+			
+		}
+				
+		return (conn != null ? true: false);
+					
+			
+	}
+	
+	public void setQuery(String newQuery) {
+		
+		this.query = newQuery;
+		
+	}
+	
+	public void testQuery() { //for debugging and trying queries
+	
+		//getting table names
+		try {
+			
+			this.rs = exQ.executeQuery(this.query);
+			
+			System.out.println("Tables: ");
+			
+			while(this.rs.next()) {
+				
+				System.out.println(this.rs.getString("name"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 	
 	public boolean addResult(Exam result) {
 		
+		//Replace with values in db
+		
+		//setQuery("INSERT INTO Table (name) VALUES ('')");
+		//this.exQ.execute(this.query);
+		
+		//check if it worked, then return true
+		//else return false
 		return true;
 	}
+	
 	
 	
 	public void editResult(Exam result, int id) {
