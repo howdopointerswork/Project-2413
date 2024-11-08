766dca9c9672baadfb40f098d77744b1ec05d00e
Orion Jamal <orionjamal@gmail.com>
2024-11-08 01:51:10 -0800

    Updated lib

---
+++ BloodTest.java
@@ -0,0 +1,28 @@
+package project2413;
+
+
+public class BloodTest extends Exam{
+	
+	
+	public int parentExamID;
+	
+	private String category;
+	
+	
+	
+	BloodTest(String date, int id, String category, int status, int User_ID){
+		
+		super(date, id, "Blood", status, User_ID);
+		
+	
+		
+	}
+	
+	
+	/*public void enterResults(String bloodTestType, String testResult) {
+		
+		return;
+		
+	}*/
+	
+}
